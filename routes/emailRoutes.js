10729fe3a5e5c4fd6083b781c00942881edd5999
burhanali2812 <syedburhanali830@gmail.com>
2026-05-16 16:09:48 +0500

    first reviews

--- routes/emailRoutes.js
+++ routes/emailRoutes.js
@@ -16,6 +16,12 @@ const transporter = nodemailer.createTransport({
     pass: process.env.SMTP_PASS,
   },
 });
+console.log("SMTP Config:", {
+  host: process.env.SMTP_HOST,
+  port: process.env.SMTP_PORT,
+  user: process.env.SMTP_USER,
+  pass: process.env.SMTP_PASS ? "******" : "Not Set",
+});
 
 function generateOTP() {
   const chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
