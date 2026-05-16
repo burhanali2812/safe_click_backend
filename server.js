2bdf56cc873c73380b28b37f1cef6a50a9ff0c30
burhanali2812 <syedburhanali830@gmail.com>
2026-05-16 16:14:28 +0500

    first reviews

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(express.json());
 // Import routes
 const userRoutes = require("./routes/userRoutes");
 const adminRoutes = require("./routes/adminRoutes");
-const emailTemplateRoutes = require("./routes/emailTemplateRoutes");
+const emailTemplateRoutes = require("./routes/emailRoutes");
 
 const PORT = process.env.PORT || 5000;
 app.get("/", (req, res) => {
